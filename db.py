076c7a4ed76fc8d87699af13fa57fda43a97057a
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2021-11-28 10:57:59 +0100

    'update'

---
+++ db.py
@@ -0,0 +1,41 @@
+import asyncio
+import sqlite3
+from datetime import datetime
+
+import psutil
+
+
+class Database:
+    def __init__(self):
+        self.conn = conn = sqlite3.connect('/app/db/service.db')
+        self.c = conn.cursor()
+
+        self.c.execute("""CREATE TABLE IF NOT EXISTS datatable(id INTEGER PRIMARY KEY AUTOINCREMENT, curr_cpu_freq INTEGER, 
+                available_ram FLOAT, used_ram FLOAT, available_swap FLOAT, used_swap FLOAT, timestamp DATE)""")
+
+        self.loop = asyncio.get_event_loop()
+        self.task = self.loop.create_task(self.update())
+
+    async def update(self):
+        while True:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print('Adding value to db', timestamp)
+            self.c.execute(
+                "INSERT INTO datatable (curr_cpu_freq, available_ram, used_ram, available_swap, used_swap, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
+                (round(psutil.cpu_freq().current), round(psutil.virtual_memory().available / 1073741824, 2),
+                 round(psutil.virtual_memory().used / 1073741824, 2),
+                 round(psutil.swap_memory().free / 1073741824, 2), round(psutil.swap_memory().used / 1073741824, 2),
+                 timestamp))
+            self.conn.commit()
+            await asyncio.sleep(1)
+
+    async def get_data(self):
+        print('Data accesed', str(datetime.now()))
+        return {'Collected data': [{'cpufreq': row[0],
+                                    'availram': row[1],
+                                    'usedram': row[2],
+                                    'availswap': row[3],
+                                    'usedswap': row[4],
+                                    'timestamp': row[5]} for row in
+                                   self.c.execute(
+                                       """SELECT * FROM (SELECT curr_cpu_freq, available_ram, used_ram, available_swap, used_swap, timestamp FROM datatable ORDER BY timestamp DESC LIMIT 720) ORDER BY timestamp ASC""")]}
